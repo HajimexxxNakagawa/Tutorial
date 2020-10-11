19857ead95850845967d176c536d6f705b7a8389
Hajime Nakagawa <navyblue0914@icloud.com>
2020-10-11 09:26:07 +0900

    Tutorial 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,13 @@ module.exports = {
     creator: "Hajime",
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
     `gatsby-plugin-emotion`,
     {
       resolve: `gatsby-plugin-typography`,
